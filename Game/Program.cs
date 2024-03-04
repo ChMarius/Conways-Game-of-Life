a7b7c7038b5d342401f9e425eda014bdca460c4d
danssolutions <danssolutionshop@gmail.com>
2024-03-04 12:32:47 +0100

    Added Program.cs

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace GameOfLife
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Conway's Game of Life");
+        }
+    }
+}
