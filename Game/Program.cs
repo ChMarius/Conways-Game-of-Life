d3dfbd9f6c372cc1bbe273b32a6a388e7648ae0e
danssolutions <danssolutionshop@gmail.com>
2024-03-04 13:08:48 +0100

    Added main user interface w/ stubs

--- Game/Program.cs
+++ Game/Program.cs
@@ -6,7 +6,35 @@ namespace GameOfLife
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Conway's Game of Life");
+            bool continueDisplay = true;
+
+            while (continueDisplay)
+            {
+                Console.WriteLine("\nConway's Game of Life");
+                Console.WriteLine("1. Create a new random grid");
+                Console.WriteLine("2. Load grid from JSON file");
+                Console.WriteLine("3. Exit");
+                Console.Write("Select an option: ");
+
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("\nNo");
+                        break;
+                    case "2":
+                        Console.WriteLine("\nHell nah");
+                        break;
+                    case "3":
+                        Console.WriteLine("\nExiting...");
+                        continueDisplay = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid option. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
