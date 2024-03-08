0f0fe9366b5783a986f0a5c203568550546c5947
Petar <petardreznjak03@gmail.com>
2024-03-08 16:38:40 +0100

    AutomatonSimulator work

--- Game/AutomatonSimulator.cs
+++ Game/AutomatonSimulator.cs
@@ -1,1 +1,29 @@
-//
+using System.Data;
+
+namespace GameOfLife
+{
+
+    public class AutomatonSimulator
+    {
+        public Grid CurrentGrid;
+    
+
+
+        static void Updategrid()
+        {
+
+
+            Grid grid = new(Rows, Columns, true);
+            Grid newGrid = grid;
+
+            for(int i = 0; i < grid.Columns; i++)
+            {
+                for(int j = 0; j < grid.Rows; j++)
+                {
+                    int neighbors = grid.GetLiveCellNeighborCount(i,j);
+                    bool cellState = grid.Cells[i][j].GetCellState();
+                }
+            }
+        }
+    }
+}
